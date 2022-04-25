276982e091221e3942e8d566d286049d091bcd63
MooMiHahda <mooey84@hotmail.com>
2022-04-25 15:08:26 +0800

    first version of openfileshare checking script

---
+++ OpenFileShare checking script.ps1
@@ -0,0 +1,13 @@
+#OpenFileShare checking script
+
+#Check for Open Share
+$SMBEveryoneSharePath = (Get-SmbShare | Get-SmbShareAccess |Where-Object {$_.accountname -eq "everyone"}|Get-SmbShare).path
+
+#Loop thru and confirm NTFS permission is also everyone
+foreach ($FileShare in $SMBEveryoneSharePath){
+
+    if(((Get-Acl $FileShare).access).IdentityReference -like "*everyone*" ){
+            return "OpenFileShareFound"
+        }    
+}
+return 0
